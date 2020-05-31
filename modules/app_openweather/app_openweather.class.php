8e08e5be12aad84c90151e4bdf334c5808550a8f
NIck <nick7zmail@gmail.com>
2020-05-31 11:54:59 +0500

    fix

--- modules/app_openweather/app_openweather.class.php
+++ modules/app_openweather/app_openweather.class.php
@@ -318,7 +318,7 @@ function usual(&$out) {
     $obj2_rec = SQLSelectOne("SELECT ID FROM objects WHERE CLASS_ID='" . $classid['ID'] . "' AND TITLE LIKE '" . DBSafe('ow_setting') . "'");
     if($obj2_rec['ID']) {
       say('Внимание, модуль Openweather был обновлён до последней версии. К сожалению нам не удалось сохранить ваши настройки, но это компенсируется тем, что модуль стал гораздо лучше и стабильнее.',2);
-      say('Пожалуйста создайте записи с местоположением в новом модуле. Напоминаем ващ API-ключ: '.gg('ow_setting.api_key').'.',2);
+      say('Пожалуйста создайте записи с местоположением в новом модуле. Напоминаем ваш API-ключ: '.gg('ow_setting.api_key').'.',2);
       say('Attention, the Openweather module has been updated to the latest version. Unfortunately, we were unable to save your settings, but the module has become much better and more stable.', 2);
       say('Please create entries in the new module. We remind you of your API key: '.gg (' ow_setting.api_key ').'. ', 2);
       debmes('Внимание, модуль Openweather был обновлён до последней версии. К сожалению нам не удалось сохранить ваши настройки, но это компенсируется тем, что модуль стал гораздо лучше и стабильнее. Пожалуйста создайте записи с местоположением в новом модуле. Напоминаем ващ API-ключ: '.gg('ow_setting.api_key').'.');
